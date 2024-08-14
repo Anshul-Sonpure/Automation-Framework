2d05cee0a9a82596756099607273942bc6cb0ed9
Anshul Sonpure <thealchemist20@protonmail.com>
2024-08-14 22:50:45 +0530

    get Users

--- src/main/java/api/ApiClient.java
+++ src/main/java/api/ApiClient.java
@@ -7,6 +7,9 @@ import io.restassured.response.Response;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
 
 public class ApiClient {
 
@@ -62,6 +65,22 @@ public class ApiClient {
         // Convert headers to a map (use a utility method)
         Map<String, String> headersMap = headers.asList().stream()
             .collect(Collectors.toMap(Header::getName, Header::getValue));
-        return new ApiResponse(response.getStatusCode(), response.getBody().asString(), headersMap);
+       
+     // Format the response body as pretty-printed JSON
+        String formattedBody = formatJson(response.getBody().asString());
+
+        return new ApiResponse(response.getStatusCode(), formattedBody, headersMap);
+        
     }
+    private String formatJson(String jsonString) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+            Object json = objectMapper.readValue(jsonString, Object.class);
+            return objectMapper.writeValueAsString(json);
+        } catch (Exception e) {
+            return jsonString; // Return the original string if formatting fails
+        }
+    }
+    
 }
--- src/test/java/apiTest/TestGetRequest.java
+++ src/test/java/apiTest/TestGetRequest.java
@@ -11,16 +11,29 @@ import utils.ConfigManager;
 
 public class TestGetRequest {
 	
+	String apiUrl = ConfigManager.getConfigValue("api.base.url");
+	ApiClient client = new ApiClient(apiUrl);
+	Map<String, String> headers = new HashMap<>();
+    
+	
+	
 	@Test
-	public void getRequestTest()
+	public void getSingleUser()
 	{
-		String apiUrl = ConfigManager.getConfigValue("api.base.url");
-		ApiClient client = new ApiClient(apiUrl);
-
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Accept", "application/json");
-
+		headers.put("Accept", "application/json");
         ApiRequest request = new ApiRequest("GET", "/users/1", null, headers);
+        ApiResponse response = client.sendRequest(request);
+
+        System.out.println("Status Code: " + response.getStatusCode());
+        System.out.println("Response Body: " + response.getBody());
+        System.out.println("Response Headers: " + response.getHeaders());
+	}
+	
+	@Test
+	public void getAllUsers()
+	{
+		headers.put("Accept", "application/json");
+        ApiRequest request = new ApiRequest("GET", "/users", null, headers);
         ApiResponse response = client.sendRequest(request);
 
         System.out.println("Status Code: " + response.getStatusCode());
